80c8a02fca4aa79538b25b36bbdf9a95c15e0acc
afsanehgh <afsaneh.gh@gmail.com>
2026-08-12 12:10:15 +1000

    Fix ns2 config: correct listen-on, ACL, and reverse zones for new router topology

--- scripts/test-reverse.sh
+++ scripts/test-reverse.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Reverse lookup (ns1):"
-dig @10.128.10.11 -x 10.128.100.101
+dig @10.128.10.11 -x 10.128.20.101
 
 echo
 echo "Reverse lookup (ns2):"
-dig @10.128.20.12 -x 10.128.100.101
+dig @10.128.10.12 -x 10.128.20.102
